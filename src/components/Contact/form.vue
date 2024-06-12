8246db5f20be8e8f783214c229ef30d04c43c711
dakeem05 <edidiongsamuel14@gmail.com>
2024-06-12 15:44:53 +0100

    Fiexd the mailer

--- src/components/Contact/form.vue
+++ src/components/Contact/form.vue
@@ -1,9 +1,68 @@
+<script setup>
+  import { ref } from 'vue';
+  import emailjs from 'emailjs-com';
+
+  const form = ref({
+      name: '',
+      email: '',
+      phone: '',
+      message: ''
+    });
+
+    const responseMessage = ref('');
+
+const sendEmail = () => {
+  const templateParams = {
+    name: form.value.name,
+    email: form.value.email,
+    phone: form.value.phone,
+    message: form.value.message,
+    to_name: "Mr. Okemiri",
+    reply_to:"edidiongsamuel14@gmail.com",
+    to_email:"edidiongsamuel14@gmail.com"
+  };
+
+  emailjs.send('service_5lguqnn', 'template_06a8d4c', templateParams, 'zllEYbgyaDL6IJFHQ')
+    .then(response => {
+    form.value.name = ''
+    form.value.email = ''
+    form.value.phone = ''
+    form.value.message = ''
+
+    responseMessage.value = 'Message sent successfully!';
+    setTimeout(() => {
+      
+      responseMessage.value = '';
+    }, 5000);
+    })
+    .catch(error => {
+      console.error('FAILED...', error);
+      form.value.name = ''
+    form.value.email = ''
+    form.value.phone = ''
+    form.value.message = ''
+      responseMessage.value = 'Failed to auto-send message. Redirecting to email app...';
+    setTimeout(() => {
+      
+      responseMessage.value = '';
+    }, 5000);
+      redirectToEmailApp(templateParams);
+    });
+};
+
+const redirectToEmailApp = (params) => {
+      const { name, email, message, phone, to_email } = params;
+      const subject = encodeURIComponent('Contact Us Form Submission');
+      const body = encodeURIComponent(`Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`);
+      const mailtoLink = `mailto:${to_email}?subject=${subject}&body=${body}`;
+      window.location.href = mailtoLink;
+    };
+</script>
+
 <template>
   <div>
     <form
-      action="mailto:your-email@example.com"
-      method="post"
-      enctype="text/plain"
+      @submit.prevent="sendEmail"
       class="w-full bg-white box text-black p-5 lg:p-10 lg:w-[582px] h-[742px] rounded-[14.85px]"
     >
       <h3 class="font-semibold font-plus-jakarta-sans text-[20px]">Contact Form</h3>
@@ -14,6 +73,7 @@
             type="text"
             id="fullName"
             name="fullName"
+            v-model="form.name"
             class="border-[0.3px] border-[#00000066] rounded-[6.1px] w-full bg-[#8A938D0D] p-3"
             required
           />
@@ -24,6 +84,7 @@
             type="email"
             id="email"
             name="email"
+            v-model="form.email"
             class="border-[0.3px] border-[#00000066] rounded-[6.1px] w-full bg-[#8A938D0D] p-3"
             required
           />
@@ -34,6 +95,7 @@
             type="tel"
             id="phone"
             name="phone"
+            v-model="form.phone"
             class="border-[0.3px] border-[#00000066] rounded-[6.1px] w-full bg-[#8A938D0D] p-3"
             required
           />
@@ -43,11 +105,13 @@
           <textarea
             id="message"
             name="message"
+            v-model="form.message"
             class="border-[0.3px] border-[#00000066] rounded-[6.1px] w-full bg-[#8A938D0D] p-3"
             rows="6"
           ></textarea>
         </div>
       </div>
+      <div v-if="responseMessage">{{ responseMessage }}</div>
       <div class="flex justify-end pt-5">
         <button
           type="submit"
