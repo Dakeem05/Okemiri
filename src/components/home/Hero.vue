f8f6e5ab6ed6c3073f94a12343c6a55e7fe3a504
Uwakmfon-Ekong <uwakmfone470@gmail.com>
2024-06-13 00:30:03 +0100

    merged conflict

--- src/components/home/Hero.vue
+++ src/components/home/Hero.vue
@@ -2,12 +2,7 @@
 </script>
 
 <template>
-<<<<<<< HEAD
   <main class="flex flex-col lg:flex-row gap-10">
-=======
-   <!-- <section class="max-w-7xl relative flex flex-col lg:flex-row justify-between "> -->
-  <main class="flex flex-col  lg:flex-row">
->>>>>>> 6ec97c00fff12179e64264096820a6fe02530f19
     <div class="w-full lg:w-[50%] pt-[2rem]">
       <div class="ml-auto w-full lg:w-[90%] px-10 lg:px-0 ">
         <h1 class="font-[700] text-[2rem] lg:text-[3rem] font-plus-jakarta-sans ">Welcome to Okemiri Hose Your Trusted Partner.</h1> <!-- Reduced font size -->
@@ -15,7 +10,6 @@
       </div>
     </div>
     <div class="w-full lg:w-[50%]">
-<<<<<<< HEAD
       <div class="px-10 lg:px-0">
         <img src="../../assets/images/Rectangle 1.png" class="object-cover h-fit lg:h-[100vh] w-full" alt="Hero Img">
       </div>
@@ -23,18 +17,6 @@
     </div>
   </main>
 </template>
-=======
-    <div class="px-10 lg:px-0">
-        <img src="../../assets/images/Rectangle 1.png" class="object-cover h-fit w-fit" alt="Hero Img">
-       
-    </div>
-     <div class="border-[1.5px] border-gray-400 mt-5 lg:hidden block"></div>
-     </div>
-
-  </main>
-  <!-- </section> -->
-</template>    
->>>>>>> 6ec97c00fff12179e64264096820a6fe02530f19
 
 <style scoped>
 /* Add any additional styles if necessary */
